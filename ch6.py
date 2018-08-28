3c2a6a2634c5371fd58c6cbde9325cb9651c6e69
rumendo <rumen.velichkovv@gmail.com>
2018-08-28 18:45:26 +0300

    Chapter 6, 7 ex.

---
+++ ch6.py
@@ -0,0 +1,19 @@
+# First exercise
+A = "spam"
+B = A
+B = "shrubbery"
+print(A)
+
+
+# Second exercise
+A = ["spam"]
+B = A
+B[0] = "shruberry"
+print(A)
+
+
+# Third exercise
+A = ["spam"]
+B = A[:]
+B[0] = "shruberry"
+print(A)
---
+++ ch7.py
@@ -0,0 +1,3 @@
+# A = ["spam", "spam", "spam", "spam"]
+# print(A.find("am"))
+# # 'list' object has no attribute 'find'
